7f60f66591c6c7abe0535623ff8daca2317ae707
Ben Gordon <benharrisgordon@gmail.com>
2020-11-11 18:18:16 +0100

    Yelp MVP Incomplete

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurants = @restaurant
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+
+end
